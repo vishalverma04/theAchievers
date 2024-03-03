10bea92c667b67cc4fce17be0b4353086323eb2c
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-03-03 20:31:39 +0530

    add admin signin signout functionality

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-const DB_NAME="TheAchieversDB"
-const connectDB= async ()=>{
-    try{
-let connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
- console.log(`/n mongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
-    } catch(error){
-    console.log("mongodb connection error: ",error)
-    process.exit(1)
+const DB_NAME = "TheAchieversDB"
+const connectDB = async () => {
+    try {
+        let connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`/n mongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("mongodb connection error: ", error)
+        process.exit(1)
     }
-} 
+}
 
 export default connectDB
--- backend/src/server.js
+++ backend/src/server.js
@@ -13,4 +13,4 @@ connectDB()
 })
 .catch((err)=>{
   console.log("mongodb connection failed !!! :",err)
-})
+}) 
