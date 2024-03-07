95323f735bea116395d3706b240c1c29b782efec
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-03-07 15:28:36 +0530

    Fixed bugs

---
+++ backend/src/models/rebate.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const rebateSchema = new mongoose.Schema({
+    complainter:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },  
+    reason : {
+        type : String, 
+        required : true, 
+    }, 
+    dateFrom : {
+        type : Date,
+        required : true
+    }, 
+    dateTo : {
+        type:Date,
+        required : true
+    }
+},{timestamps:true})
+
+export const Rebate = mongoose.model("Rebate",rebateSchema) 
